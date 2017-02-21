d68a984e6c1014bd78787346635e4a56be0b994e
David <6000827@S4N99288.insags.com>
2017-02-21 14:51:16 +0100

    Added Horas_costes classes and controller

---
+++ src/main/java/com/becarios/proyecto_definitivo/model/horas_costes/ValoracionFinal.java
@@ -0,0 +1,32 @@
+package com.becarios.proyecto_definitivo.model.horas_costes;
+
+public class ValoracionFinal {
+    private String nombre;
+    private int horas;
+    private int coste;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public int getCoste() {
+        return coste;
+    }
+
+    public void setCoste(int coste) {
+        this.coste = coste;
+    }
+
+}
