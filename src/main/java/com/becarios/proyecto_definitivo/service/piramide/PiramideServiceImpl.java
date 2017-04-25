388aeb248ac3b2120ecb2363ef2e6313759a38c5
marius <6000826@S4N99978.insags.com>
2017-04-25 16:03:39 +0200

    piramide

--- src/main/java/com/becarios/proyecto_definitivo/service/piramide/PiramideServiceImpl.java
+++ src/main/java/com/becarios/proyecto_definitivo/service/piramide/PiramideServiceImpl.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.becarios.proyecto_definitivo.service.piramide;
 
 import java.util.List;
@@ -35,48 +34,4 @@ public class PiramideServiceImpl implements PiramideService {
 	}
 
 }
-=======
-// package com.becarios.proyecto_definitivo.service.piramide;
-//
-// import java.util.List;
-//
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
-// import org.springframework.transaction.annotation.Transactional;
-//
-// import com.becarios.proyecto_definitivo.dao.Piramide.PiramideDao;
-// import com.becarios.proyecto_definitivo.dao.factores_ajustes.FactorAjusteDao;
-// import
-// com.becarios.proyecto_definitivo.model.factores_ajustes.ArquitecturaReferencia;
-// import com.becarios.proyecto_definitivo.model.factores_ajustes.FactorAjuste;
-// import
-// com.becarios.proyecto_definitivo.model.factores_ajustes.FactorComplejidadAmbiental;
-// import com.becarios.proyecto_definitivo.model.piramide.Piramide;
-// import com.becarios.proyecto_definitivo.model.piramide.PiramideId;
-//
-// @Service("piramidesService")
-// @Transactional
-// public class PiramideServiceImpl implements PiramideService {
-//
-// @Autowired
-// private PiramideDao dao;
-//
-// @Override
-// public List<Piramide> findAllById(int id) {
-// return PiramideDao.findAllPiramid(new PiramideId(nombre, id));
-// }
-//
-// @Override
-// boolean void savePiramide(Piramide piramide) {
-// PiramideDao.savePiram(piramide);
-// return true;
-// }
-//
-// @Override
-// public List<Piramide> findPiramideById(int id) {
-// // TODO Auto-generated method stub
-// return null;
-// }
-//
-// }
->>>>>>> branch 'finish' of https://github.com/ShadowFox95/Herramienta_Estimacion.git
+
